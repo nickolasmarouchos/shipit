be4d93dd0b7db46b4b5605db36e23734d1f5a5a8
Hendrik Demmer <hendrik.demmer@gmail.com>
2017-01-21 13:12:38 +0100

    water

--- js/ocean.js
+++ js/ocean.js
@@ -1,5 +1,5 @@
 
-var NUM_WATER_SEGMENTS = pixWidth/8+1;
+var NUM_WATER_SEGMENTS = pixWidth;
 
 var waterLevels = [];
 (function()
@@ -10,10 +10,27 @@ var waterLevels = [];
     }
 })();
 
-function updateWater()
+function updateWater() {
+    for (var i=0;i<2;i++) {
+        updateWaterTick();
+    }
+}
+
+function updateWaterTick()
 {
-    for (var i=0;i<NUM_WATER_SEGMENTS;i++) {
-        waterLevels[i] = Math.sin(i/2.0 - time/500) * 5;
+    var leftMost = Math.cos(time / 100) * Math.cos(time/1000) * 10;
+
+    for (var i=NUM_WATER_SEGMENTS-1;i>=0;i--) {
+
+        var l = 0;
+        if (i>0)
+        {
+            l=waterLevels[i-1];
+        } else{
+            l=leftMost;
+        }
+
+        waterLevels[i] = l;
     }
 }
 
