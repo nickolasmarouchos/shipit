b123a46be85bcc7870dde9c92b31f3dbaf08951a
Hendrik Demmer <hendrik.demmer@gmail.com>
2017-01-21 13:26:01 +0100

    Slower water

--- js/ocean.js
+++ js/ocean.js
@@ -11,13 +11,6 @@ var waterLevels = [];
 })();
 
 function updateWater() {
-    for (var i=0;i<2;i++) {
-        updateWaterTick();
-    }
-}
-
-function updateWaterTick()
-{
     var leftMost = Math.cos(time / 100) * Math.cos(time/1000) * 10;
 
     for (var i=NUM_WATER_SEGMENTS-1;i>=0;i--) {
