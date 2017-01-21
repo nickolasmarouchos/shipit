89ee2b58e83a8b8fb0cfa2b2f013557919b49d1d
Hendrik Demmer <hendrik.demmer@gmail.com>
2017-01-21 20:54:33 +0100

    fix sailor display

--- js/boatRender.js
+++ js/boatRender.js
@@ -26,12 +26,12 @@ function drawBoats()
             {
                 for(var slr = 0; slr<part.sailors.length; slr++)
                 {
-                    sailorCount++;
-                    if ( sailorCount >= boat.sailorOffset && sailorCount < boat.hp + boat.sailorOffset) {
+                    if ( boat.sailors.indexOf(sailorCount) != -1) {
                         var slrx = x - config.width / 2 + part.sailors[slr].x;
                         var slry = boat.yHist[1] + part.sailors[slr].y;
                         drawSprite(sailor, slrx, slry);
                     }
+                    sailorCount++;
                 }
             }
             drawSprite(part.img, x, y);
