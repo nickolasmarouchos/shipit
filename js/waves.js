6e3d36e9ae7621c41ea9ca488d29c3390dd35683
Hendrik Demmer <hendrik.demmer@gmail.com>
2017-01-21 15:14:33 +0100

    safety

--- js/waves.js
+++ js/waves.js
@@ -1,11 +1,13 @@
 
 var simpleWave = [];
 
-var LENGTH = 100;
+(function() {
+    var LENGTH = 100;
 
-for (var i=0; i<LENGTH;i++)
-{
-    var y =Math.sin(i/10);
-    y *= (LENGTH - i) / LENGTH
-    simpleWave.push(y);
-}
+    for (var i = 0; i < LENGTH; i++) {
+        var y = Math.sin(i / 10);
+        y *= (LENGTH - i) / LENGTH
+        simpleWave.push(y);
+    }
+    
+})();
