98ee9d1701fd10e1c37eefd4db768b118a507f6b
Nickolas Marouchos <nickolasmarouchos@Nickolass-MacBook-Pro.local>
2017-01-21 13:37:27 +0100

    Split up rendering front-back for ocean/boat

---
+++ js/boatRender.js
@@ -0,0 +1,21 @@
+
+function drawBoatBack()
+{
+    //Draw people behind boat
+}
+
+function drawBoat()
+{
+    //Draw main boat
+    var seaLevel = 40;
+
+    var x = pixWidth/2;
+    var y = seaLevel;
+    drawSprite(boat1, x,y);
+}
+
+
+function drawBoatFront()
+{   
+    //Draw people in front of boat
+}
